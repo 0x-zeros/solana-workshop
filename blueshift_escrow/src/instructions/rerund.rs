d219e26fb108faf7f4eebe731396ce151a02b795
zeros <zeros.null@gmail.com>
2026-02-04 18:04:26 +0800

    task5 修改

---
+++ blueshift_escrow/src/instructions/rerund.rs
@@ -0,0 +1,127 @@
+pub struct Rerund<'a> {
+    pub accounts: RerundAccounts<'a>,
+}
+
+impl<'a> Rerund<'a> {
+    pub const DISCRIMINATOR: &'a u8 = &2;
+
+    pub fn process(&mut self) -> ProgramResult {
+        let data = self.accounts.escrow.try_borrow_data()?;
+        let escrow = Escrow::load(&data)?;
+
+        // Check if the escrow is valid
+        let escrow_key = create_program_address(
+            &[
+                b"escrow",
+                self.accounts.maker.key(),
+                &escrow.seed.to_le_bytes(),
+                &escrow.bump,
+            ],
+            &crate::ID,
+        )?;
+        if &escrow_key != self.accounts.escrow.key() {
+            return Err(ProgramError::InvalidAccountOwner);
+        }
+
+        //todo 为什么没有检测vault是否是escrow的associated token account?
+
+        let seed_binding = escrow.seed.to_le_bytes();
+        let bump_binding = escrow.bump;
+        let escrow_seeds = [
+            Seed::from(b"escrow"),
+            Seed::from(self.accounts.maker.key().as_ref()),
+            Seed::from(&seed_binding),
+            Seed::from(&bump_binding),
+        ];
+        let signer = Signer::from(&escrow_seeds);
+
+        let amount = TokenAccount::from_account_info(self.accounts.vault)?.amount();
+
+        //将代币 A 的全部余额从保险库转回创建者，然后关闭保险库账户。
+
+        // Transfer from the Vault to the Maker
+        Transfer {
+            from: self.accounts.vault,
+            to: self.accounts.maker_ata_a,
+            authority: self.accounts.escrow,
+            amount,
+        }
+        .invoke_signed(&[signer.clone()])?;
+
+        // Close the Vault
+        CloseAccount {
+            account: self.accounts.vault,
+            destination: self.accounts.maker,
+            authority: self.accounts.escrow,
+        }
+        .invoke_signed(&[signer.clone()])?;
+
+        //关闭托管 PDA，并将其租金 lamports 返还给创建者。
+
+        // Close the Escrow
+        drop(data);
+        ProgramAccount::close(self.accounts.escrow, self.accounts.maker)?; //lamports 返还给了谁？
+
+        Ok(())
+    }
+}
+
+impl<'a> TryFrom<&'a [AccountInfo]> for Rerund<'a> {
+    type Error = ProgramError;
+
+    fn try_from(accounts: &'a [AccountInfo]) -> Result<Self, Self::Error> {
+        let accounts = RerundAccounts::try_from(accounts)?;
+
+        // Initialize necessary accounts
+        AssociatedTokenAccount::init_if_needed(
+            accounts.maker_ata_a,
+            accounts.mint_a,
+            accounts.maker,
+            accounts.maker,
+            accounts.system_program,
+            accounts.token_program,
+        )?;
+
+        Ok(Self { accounts })
+    }
+}
+
+pub struct RerundAccounts<'a> {
+    pub maker: &'a AccountInfo,
+    pub escrow: &'a AccountInfo,
+    pub mint_a: &'a AccountInfo,
+    pub vault: &'a AccountInfo,
+    pub maker_ata_a: &'a AccountInfo,
+    pub system_program: &'a AccountInfo,
+    pub token_program: &'a AccountInfo,
+}
+
+impl<'a> TryFrom<&'a [AccountInfo]> for RerundAccounts<'a> {
+    type Error = ProgramError;
+
+    fn try_from(accounts: &'a [AccountInfo]) -> Result<Self, Self::Error> {
+        let [maker, escrow, mint_a, vault, maker_ata_a, system_program, token_program, _] =
+            accounts
+        else {
+            return Err(ProgramError::NotEnoughAccountKeys);
+        };
+
+        // Basic Accounts Checks
+        SignerAccount::check(maker)?;
+        ProgramAccount::check(escrow)?;
+        MintInterface::check(mint_a)?;
+        AssociatedTokenAccount::check(vault, escrow, mint_a, token_program)?;
+        AssociatedTokenAccount::check(maker_ata_a, maker, mint_a, token_program)?;
+
+        // Return the accounts
+        Ok(Self {
+            maker,
+            escrow,
+            mint_a,
+            vault,
+            maker_ata_a,
+            system_program,
+            token_program,
+        })
+    }
+}
